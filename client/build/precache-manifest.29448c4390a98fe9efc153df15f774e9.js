5725bce7e2a424c0697f6bfd19308615898402a3
akshat2906 <91730293+akshat2906@users.noreply.github.com>
2023-01-11 13:04:19 +0530

    Initial commit

---
+++ client/build/precache-manifest.29448c4390a98fe9efc153df15f774e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f710562f55533a1c56d9089caf985e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b30310b9788f7f0e01ba",
+    "url": "/static/css/main.a8e2a76e.chunk.css"
+  },
+  {
+    "revision": "08bfe183196f03331ccd",
+    "url": "/static/js/2.ed1285a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed1285a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b30310b9788f7f0e01ba",
+    "url": "/static/js/main.e5e5aeda.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
